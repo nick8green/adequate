136dc6f7bef064e9912806995f614ae2b53857ca
Nick Green <nick@nick8green.co.uk>
2025-06-15 22:15:31 +0100

    ci: updated docker setup

--- apps/app/docker-entrypoint.sh
+++ apps/app/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-node_modules/.bin/style-dictionary build --config /tools/designs/config.js
+/designs/node_modules/.bin/style-dictionary build --config /designs/config.js
 node_modules/.bin/next start
